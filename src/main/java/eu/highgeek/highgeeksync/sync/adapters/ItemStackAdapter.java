b366b7642b61a4b6b6b89fcea79f1faaff528e65
Radim Goliáš <ragoli66@gmail.com>
2024-07-24 17:47:49 +0200

    itemstack parser using eco

--- src/main/java/eu/highgeek/highgeeksync/sync/adapters/ItemStackAdapter.java
+++ src/main/java/eu/highgeek/highgeeksync/sync/adapters/ItemStackAdapter.java
@@ -1,17 +1,28 @@
 package eu.highgeek.highgeeksync.sync.adapters;
 
+import java.io.*;
+import java.nio.Buffer;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 
+import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import eu.highgeek.highgeeksync.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.NBTComponent;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import com.willfp.eco.core.items.Items;
+
 import com.saicone.rtag.item.ItemTagStream;
+import org.jetbrains.annotations.NotNull;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 //import com.willfp.eco.;;
 
@@ -19,8 +30,9 @@ import com.saicone.rtag.item.ItemTagStream;
 public class ItemStackAdapter {
     public static Gson gson = new GsonBuilder()
             .setPrettyPrinting()
+            .setLenient()
             .create();
-
+/*
     public static String itemStackToString(ItemStack itemStack){
         if (itemStack.getType() == Material.AIR){
             return "{\"id\":\"minecraft:air\"}";
@@ -46,6 +58,35 @@ public class ItemStackAdapter {
             }
         }
     }
+    */
+
+
+    public static String itemStackToString(ItemStack itemStack){
+        if (itemStack.getType() == Material.AIR){
+            return "{\"id\":\"minecraft:air\"}";
+        }
+        return Items.toSNBT(itemStack);
+    }
+
+    public static ItemStack stringToItemStack(String json){
+        Map<String, Object> readableMap = gson.fromJson(json, Map.class);
+
+        if(readableMap.get("id").equals("minecraft:air")){
+            return new ItemStack(Material.AIR);
+        }else {
+            try {
+                ItemStack itemStack = Items.fromSNBT(json);
+                if(itemStack.getType() == Material.AIR){
+                    return returnErrorBarrier();
+                }
+                return Objects.requireNonNullElseGet(itemStack, ItemStackAdapter::returnErrorBarrier);
+            }catch (Exception e){
+                Main.logger.warning("stringToItemStack exception: " + e.getMessage() );
+                return returnErrorBarrier();
+            }
+        }
+    }
+
 
     public static ItemStack returnErrorBarrier(){
         ItemStack item = new ItemStack(Material.BARRIER);
